65e6669d4b71df6a45999f2b6c5b1978706478a3
zhiwei.deng@okcoin.com <zhiwei.deng@okcoin.com>
2017-04-20 00:52:44 +0800

    初始化项目完成

---
+++ src/main/java/com/easytoolsoft/commons/lang/StringUtils.java
@@ -0,0 +1,8 @@
+package com.easytoolsoft.commons.lang;
+
+/**
+ * @author zhiwei.deng
+ * @date 2017-04-20
+ **/
+public class StringUtils extends org.apache.commons.lang3.StringUtils {
+}
---
+++ src/test/java/com/easytoolsoft/commons/lang/StringUtilsTest.java
@@ -0,0 +1,11 @@
+package com.easytoolsoft.commons.lang;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+
+/**
+ * @author zhiwei.deng
+ * @date 2017-04-20
+ **/
+@Ignore
+public class StringUtilsTest {
+}
